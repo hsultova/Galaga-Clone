c52f549ec119bf3502bca0273db690798e7dad75
hsultova <hsultova@gmail.com>
2020-02-07 00:14:05 +0200

    Add ship and implement left/right movement

---
+++ GalagaClone/Assets/Code/Ship.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+	public float speed = 2f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+	}
+}
