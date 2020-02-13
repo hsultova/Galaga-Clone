3895be2c055d3f46d8d8394aa3fd5fb8015d6ae3
hsultova <hsultova@gmail.com>
2020-02-13 23:06:06 +0200

    Add basic collision and wrap enemies on the screen
    If there is collision between enemy and ship, destroy them.
    If there is collision between enemy and bullet, destroy them.

--- GalagaClone/Assets/Code/Bullet.cs
+++ GalagaClone/Assets/Code/Bullet.cs
@@ -20,4 +20,13 @@ public class Bullet : MonoBehaviour
 	{
 		Destroy(gameObject);
 	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.tag == "Enemy")
+		{
+			Destroy(collision.gameObject);
+			Destroy(gameObject);
+		}
+	}
 }
---
+++ GalagaClone/Assets/Code/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public float Speed = 50f;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = new Vector2(transform.position.x, transform.position.y - Speed * Time.deltaTime);
+		Vector3 down = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+		Vector3 up = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, Camera.main.nearClipPlane));
+		if (transform.position.y > up.y || transform.position.y < down.y)
+		{
+			transform.position = new Vector2(transform.position.x, -transform.position.y);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			Destroy(collision.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
