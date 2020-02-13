e9121ac7c381a0bf5e6a5965bc05ba7f6a1270ee
hsultova <hsultova@gmail.com>
2020-02-13 22:19:17 +0200

    Refactoring of the ship script

--- GalagaClone/Assets/Code/Ship.cs
+++ GalagaClone/Assets/Code/Ship.cs
@@ -2,13 +2,18 @@
 
 public class Ship : MonoBehaviour
 {
+	[Header("Ship attributes")]
 	public float Speed = 2f;
 	public float BoundsOffset = 2f;
+
+	[Header("Camera")]
 	public Camera GameCamera;
+
+	[Header("Bullet attributes")]
 	public Transform BulletPrefab;
 	public float BulletSpawnTime = 0.5f;
 
-	private float startTime;
+	private float _startTime;
 
 	// Start is called before the first frame update
 	void Start()
@@ -18,6 +23,35 @@ public class Ship : MonoBehaviour
 
 	// Update is called once per frame
 	void Update()
+	{
+		Move();
+		Fire();
+	}
+
+	private void Fire()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(BulletPrefab, transform);
+			_startTime = Time.time;
+		}
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			if (Time.time - _startTime > BulletSpawnTime)
+			{
+				Instantiate(BulletPrefab, transform);
+				_startTime = Time.time;
+			}
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			_startTime = 0;
+		}
+	}
+
+	private void Move()
 	{
 		Vector3 left = GameCamera.ViewportToWorldPoint(new Vector3(0, 0, GameCamera.nearClipPlane));
 		Vector3 right = GameCamera.ViewportToWorldPoint(new Vector3(1, 0, GameCamera.nearClipPlane));
@@ -35,26 +69,6 @@ public class Ship : MonoBehaviour
 		if (newPosition.x > left.x + BoundsOffset && newPosition.x < right.x - BoundsOffset)
 		{
 			transform.position = newPosition;
-		}
-
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Instantiate(BulletPrefab, transform);
-			startTime = Time.time;
-		}
-
-		if (Input.GetKey(KeyCode.Space))
-		{
-			if (Time.time - startTime > BulletSpawnTime)
-			{
-				Instantiate(BulletPrefab, transform);
-				startTime = Time.time;
-			}
-		}
-
-		if (Input.GetKeyUp(KeyCode.Space))
-		{
-			startTime = 0;
 		}
 	}
 }
