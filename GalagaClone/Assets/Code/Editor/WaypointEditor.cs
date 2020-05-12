4f29e1d13114f77f7d2d9585f47162ec88eb86d4
hsultova <hsultova@gmail.com>
2020-05-12 11:05:56 +0300

    Add Waypoint Editor  functionality and a test scene
    Used for generating wayponts for pattern movement

---
+++ GalagaClone/Assets/Code/Editor/WaypointEditor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class WaypointEditor : EditorWindow
+{
+	private GameObject _waypointPrefab;
+	private TextMesh _textPrefab;
+	private bool _canInstantiateWaypoints = true;
+	private List<Waypoint> _waypointObjects = new List<Waypoint>();
+
+	[MenuItem("Window/Waypoint Editor")]
+	public static void ShowWindow()
+	{
+		GetWindow<WaypointEditor>("Waypoint Editor");
+	}
+
+	void OnGUI()
+	{
+		_canInstantiateWaypoints = EditorGUILayout.Toggle("Instantiate Waypoints", _canInstantiateWaypoints);
+		GUILayout.Label("Choose an object to use as waypoint:", EditorStyles.boldLabel);
+		_waypointPrefab =  (GameObject) EditorGUILayout.ObjectField(_waypointPrefab, typeof(GameObject), true);
+
+		GUILayout.Label("Choose a TextMesh style:", EditorStyles.boldLabel);
+		_textPrefab = (TextMesh)EditorGUILayout.ObjectField(_textPrefab, typeof(TextMesh), true);
+	}
+
+	private void OnEnable()
+	{
+		SceneView.duringSceneGui += SceneGUI;
+	}
+
+	private void OnDisable()
+	{
+		SceneView.duringSceneGui -= SceneGUI;
+	}
+
+	private void SceneGUI(SceneView sceneView)
+	{
+		if (!_canInstantiateWaypoints)
+			return;
+
+		if (Event.current.type == EventType.MouseDown)
+		{
+			Vector3 mousePosition = Event.current.mousePosition;
+			mousePosition.y = sceneView.camera.pixelHeight - mousePosition.y;
+			mousePosition = sceneView.camera.ScreenToWorldPoint(mousePosition);
+			mousePosition.z = 0;
+
+			GameObject waypointObject = Instantiate(_waypointPrefab, mousePosition, Quaternion.identity);
+
+			TextMesh textObject = Instantiate(_textPrefab, mousePosition, Quaternion.identity);
+			textObject.transform.SetParent(waypointObject.transform);
+			textObject.text = $"{_textPrefab.text} {_waypointObjects.Count}";
+		}
+	}
+
+	private void OnHierarchyChange()
+	{
+		_waypointObjects = Resources.FindObjectsOfTypeAll<Waypoint>().ToList();
+	}
+}
---
+++ GalagaClone/Assets/Code/WaypointComponent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WaypointComponent : MonoBehaviour
+{
+	public float MoveSpeed = 5f;
+
+	private Waypoint[] _waypoints;
+	private int _waypointIndex = 0;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		_waypoints = Resources.FindObjectsOfTypeAll<Waypoint>();
+		_waypointIndex = _waypoints.Length - 1;
+
+		//to move
+		var waypoints = FindObjectsOfType<Waypoint>();
+		foreach (var waypoint in waypoints)
+		{
+			waypoint.gameObject.SetActive(false);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, _waypoints[_waypointIndex].transform.position, MoveSpeed * Time.deltaTime);
+
+		if (transform.position == _waypoints[_waypointIndex].transform.position)
+		{
+			_waypointIndex--;
+		}
+
+		if (_waypointIndex == -1)
+			_waypointIndex = _waypoints.Length - 1;
+	}
+}
